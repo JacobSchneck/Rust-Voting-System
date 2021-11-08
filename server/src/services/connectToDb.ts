8bab11c67c0da39b8ec0c9d52361c1094a93db07
Jacob Schneck <jschneck@Gerald.localdomain>
2021-11-08 15:47:09 -0600

    check point!

--- server/src/services/connectToDb.ts
+++ server/src/services/connectToDb.ts
@@ -1,7 +1,7 @@
 import sqlite3 from "sqlite3";
 sqlite3.verbose();
 
-async function connectToDb() {
+function connectToDb() {
 	const pathDb = "./db/testing_db.db"; 
 	let db = new sqlite3.Database(pathDb, ( err: any ) => {
 		if (err) return console.error(err.message);
